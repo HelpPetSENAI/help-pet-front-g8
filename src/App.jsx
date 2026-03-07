2cf2c9bad74e7993ecde12ea9b0ed978695d077a
GoBrazill <vinicius.daraujos26@gmail.com>
2026-03-06 22:18:40 -0300

    refactor: move routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ComingSoonPage from './pages/CommingSoonPage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import GlobalStyle from './styles/GlobalStyle.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ComingSoonPage />,
+    errorElement: <NotFoundPage />
+  }
+])
+
 export default function App() {
 
   return (
     <>
-      <h1>Aplicação rodando corretamente</h1>
+      <GlobalStyle />
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/reset.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ComingSoon from './pages/CommingSoonPage.jsx'
-import NotFoundPage from './pages/NotFoundPage.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <ComingSoon />,
-    errorElement: <NotFoundPage />
-  }  
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
