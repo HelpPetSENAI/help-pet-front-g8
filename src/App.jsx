ef2f23eec3bdf58fe6dc43fa2047d498afe58918
GoBrazill <vinicius.daraujos26@gmail.com>
2026-03-08 11:15:43 -0300

    chore: add explanation of react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import ComingSoonPage from './pages/CommingSoonPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 import GlobalStyle from './styles/GlobalStyle.jsx'
 
+// Criando uma array para definir as rotas do site utilizando a função createBrowserRouter importada
 const router = createBrowserRouter([
   {
     path: '/',
     element: <ComingSoonPage />,
+    // Error element aparece no caso de digitar um endereço que não existe, link com caminho errado ou erros de carregamento
     errorElement: <NotFoundPage />
   }
 ])
@@ -16,6 +18,7 @@ export default function App() {
   return (
     <>
       <GlobalStyle />
+      {/* RouterProvider fornece as rotas definidas acima para serem renderizadas através do atríbuto router */}
       <RouterProvider router={router} />
     </>
   )
