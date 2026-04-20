58ca1dc7103ae41354d67b406fa8ef6bb4eaf2b3
GoBrazill <vinicius.daraujos26@gmail.com>
2026-04-20 11:05:26 +0000

    feat: add dashboard grid style

---
+++ src/pages/g8-pages/DashboardGridStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const DashboardStyle = styled.section`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: auto;
+    gap: 1rem;
+
+    width: 100vw;
+    height: 100vh;
+    padding: 1rem;
+
+    background-color: var(--clr-green-50);
+`
